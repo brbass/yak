2e5d649fce5b93cb805577075d62511576ebf3e3
Brody Bassett <brbass@umich.edu>
2016-06-08 13:46:52 -0400

    Simple solid geometry seems to be working.

--- test/tst_geometry.cc
+++ test/tst_geometry.cc
@@ -1,8 +1,11 @@
+#include <iomanip>
+#include <iostream>
 #include <memory>
 #include <vector>
 
 #include "Circle.hh"
 #include "Line.hh"
+#include "Random_Number_Generator.hh"
 #include "Region.hh"
 #include "Solid_Geometry.hh"
 #include "Surface.hh"
@@ -62,6 +65,66 @@ void test_square()
     shared_ptr<Solid_Geometry> solid_geometry = make_shared<Solid_Geometry>(dimension,
                                                                             surfaces,
                                                                             regions);
+
+    int num_tests = 10;
+
+    Random_Number_Generator rng(-1.5, 1.5);
+
+    int w = 12;
+    
+    cout << setw(w) << "x";
+    cout << setw(w) << "y";
+    cout << setw(w) << "Direction_x";
+    cout << setw(w) << "Direction_y";
+    cout << setw(w) << "Region";
+    cout << setw(w) << "Surface";
+    cout << setw(w) << "Distance";
+    cout << setw(w) << "x_new";
+    cout << setw(w) << "y_new";
+    cout << endl;
+    
+    for (int i = 0; i < num_tests; ++i)
+    {
+        vector<double> position = rng.random_double_vector(dimension);
+        
+        vector<double> direction = rng.random_double_vector(dimension);
+        double sum = 0;
+        for (int i = 0; i < dimension; ++i)
+        {
+            sum += direction[i] * direction[i];
+        }
+        sum = sqrt(sum);
+        for (int i = 0; i < dimension; ++i)
+        {
+            direction[i] /= sum;
+        }
+
+        double distance;
+        vector<double> new_position;
+        
+        int region = solid_geometry->find_region(position);
+        int surface = solid_geometry->next_intersection(position,
+                                                        direction,
+                                                        distance,
+                                                        new_position);
+        
+        for (int i = 0; i < dimension; ++i)
+        {
+            cout << setw(w) << position[i];
+        }
+        for (int i = 0; i < dimension; ++i)
+        {
+            cout << setw(w) << direction[i];
+        }
+        cout << setw(w) << region;
+        cout << setw(w) << surface;
+        cout << setw(w) << distance;
+        for (int i = 0; i < dimension; ++i)
+        {
+            cout << setw(w) << new_position[i];
+        }
+        cout << endl;
+    }
 }
 
 void test_pincell()
