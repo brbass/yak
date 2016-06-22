cfe07fa13d367661562b976ccf1811d82c371258
Brody Bassett <brbass@umich.edu>
2016-06-22 15:23:05 -0400

    Added null solver to test geometry without the solution.

---
+++ scripts/plot_points_2d.py
@@ -0,0 +1,33 @@
+import os, sys, subprocess, itertools, multiprocessing, functools
+import numpy as np
+import xml.etree.ElementTree as et
+from matplotlib import pyplot as plt
+from matplotlib import cm as cm
+
+def plot_points_2d(file_path):
+    output_xml = et.parse(file_path).getroot()
+
+    dimension = int(output_xml.findtext("./rbf_mesh/dimension"))
+    num_points = int(output_xml.findtext("./rbf_mesh/number_of_points"))
+    points_data = np.fromstring(output_xml.findtext("./rbf_mesh/point_positions"), sep="\t")
+    material = np.fromstring(output_xml.findtext("./rbf_mesh/material"), sep="\t")
+    points = np.empty((num_points, dimension))
+    colors = cm.rainbow(material)
+    
+    for i in range(num_points):
+        for d in range(dimension):
+            points[i, d] = points_data[d + dimension * i]
+
+    plt.figure()
+    plt.scatter(points[:,0], points[:,1], c=colors)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.axis('equal')
+    plt.show()
+    
+if (len(sys.argv) != 2):
+    sys.exit()
+
+file_path = sys.argv[1]
+
+plot_points_2d(file_path)
