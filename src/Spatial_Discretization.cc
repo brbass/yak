61b22ccb59519eacf3054bbeb4c623716171755d
Brody Bassett <brbass@umich.edu>
2016-06-25 18:00:30 -0400

    Altered directory structure.

--- src/Spatial_Discretization.cc
+++
@@ -1,9 +0,0 @@
-#include "Spatial_Discretization.hh"
-
-Spatial_Discretization::
-Spatial_Discretization(int dimension,
-                       Geometry geometry):
-    dimension_(dimension),
-    geometry_(geometry)
-{
-}
