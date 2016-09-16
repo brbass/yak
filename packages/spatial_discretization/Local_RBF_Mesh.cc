f3c60131323e8bc0074d958ad2a7a6f3ddd95b98
Brody Bassett <brbass@umich.edu>
2016-09-16 15:22:33 -0400

    Changed conversion of the RBF coefficients to phi. Previously, the conversion matrix should have been transposed and was not because of the assumption that the matrix is symmetric, which is only true if the shape parameter is constant for all the neighboring points.

--- packages/spatial_discretization/Local_RBF_Mesh.cc
+++ packages/spatial_discretization/Local_RBF_Mesh.cc
@@ -84,6 +84,7 @@ Local_RBF_Mesh(int dimension,
             shared_ptr<Epetra_SerialDenseSolver> solver = make_shared<Epetra_SerialDenseSolver>();
 
             solver->SetMatrix(*mat);
+            solver->SolveWithTranspose(true);
             solver->Factor();
             
             matrices_.push_back(mat);
